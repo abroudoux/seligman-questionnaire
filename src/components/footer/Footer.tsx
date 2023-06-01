43ea713d4a2a41b7743b5e3a2c484035b0349053
abroudoux <arthur.broudoux@gmail.com>
2023-06-01 18:04:59 +0200

    commit before inputs restores values at chargement

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -20,10 +20,7 @@ export default function Footer() {
                     <a href="https://fr.linkedin.com/company/hi-is-nice" target="_blank">Linkedin</a>
                 </li>
                 <li>
-                    <a href="https://krakn-behavourial.webflow.io/formations/formation-influence-krakn-behavioural" target="_blank" rel="noreferrer">Site Web</a>
-                </li>
-                <li>
-                    <a href="mailto:patrice@krakn.fr" target="_blank" rel="noreferrer">Email</a>
+                    <a href="https://krakn-behavourial.webflow.io/formations/formation-influence-krakn-behavioural" target="_blank" rel="noreferrer">Site Internet</a>
                 </li>
             </ul>
             <p>© 2023 Krakn Behavioural. Tous droits réservés</p>
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,17 +1,20 @@
 // React
 import { Link, useRouteError } from "react-router-dom";
 
+// Style
+import './errorPage.scss';
+
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
 
     return (
-        <>
+        <div id="error-page">
             <h1>Oops!</h1>
             <p>Désolé, une erreur est apparue.</p>
 
             <Link to="/">Retourner à l'accueil</Link>
-        </>
+        </div>
     )
 }
