f43043fbcae464cd3e101992adfd7b6b0278a243
abroudoux <arthur.broudoux@gmail.com>
2023-06-05 20:53:12 +0200

    fix values prblm

--- src/components/questions/data/questions-b.ts
+++ src/components/questions/data/questions-b.ts
@@ -5,168 +5,168 @@ const questionsB = [
         question: "Je m’ennuie facilement",
         number: 1,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 26,
         question: "Je ne fais jamais d’effort pour aller dans les musées",
         number: 2,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 27,
         question: "J’ai tendance à avoir des jugements à l’emporte pièce",
         number: 3,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 28,
         question: "La plupart de mes amis ont plus d’imagination que moi",
         number: 4,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 29,
         question: " J’ai du mal à comprendre ce que les autres ressentent",
         number: 5,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 30,
         question: "On me demande rarement conseil",
         number: 6,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 31,
         question: "Je suis souvent envahi(e) par la souffrance et le découragement",
         number: 7,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 32,
         question: "Je m’écarte parfois de mon objectif",
         number: 8,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 33,
         question: "Mes amis ne me disent jamais que je suis pragmatique.",
         number: 9,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     {
         id: 34,
         question: "La chance des autres m’enthousiasme rarement autant que la mienne",
         number: 10,
         type: "b",
-        calcul: [ 1,2,3,4,5 ]
+        calcul: [ 5,4,3,2,1 ]
     },
     // {
     //     id: 35,
     //     question: "J’ai du mal à accepter l’amour des autres",
     //     number: 11,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 36,
     //     question: "J’hésite à sacrifier mes intérêts personnels au profit des groupes auxquels j'appartiens",
     //     number: 12,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 37,
     //     question: "J’ai du mal à traiter avec équité les gens que je n’aime pas",
     //     number: 13,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 38,
     //     question: "Je ne suis pas très doué pour organiser des activités collectives",
     //     number: 14,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 39,
     //     question: "J’ai du mal à suivre un régime",
     //     number: 15,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 40,
     //     question: "Il m’arrive de mal choisir mes amis ou mes relations",
     //     number: 16,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 41,
     //     question: "Je me vante souvent de ce que j’ai fait",
     //     number: 17,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 42,
     //     question: "Je n’ai rien créé de beau au cours de l’année passée",
     //     number: 18,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 43,
     //     question: "Je prends rarement la peine de réfléchir à quel point j’ai de la chance",
     //     number: 19,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 44,
     //     question: "J’ai rarement un plan réfléchi de ce que je veux faire",
     //     number: 20,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 45,
     //     question: "Je n’ai pas de vocation dans la vie",
     //     number: 21,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 46,
     //     question: "J’essaie toujours de régler mes comptes avec les autres",
     //     number: 22,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 47,
     //     question: "Je dis rarement quelque chose de drôle",
     //     number: 23,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
     // {
     //     id: 48,
     //     question: "Je broie souvent du noir",
     //     number: 24,
     //     type: "b",
-    //     calcul: [ 1,2,3,4,5 ]
+    //     calcul: [ 5,4,3,2,1 ]
     // },
 
 ];
